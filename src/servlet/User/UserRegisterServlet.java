7e2472a9368badab8d64949a002c7c6fab9b75e1
unknown <1055899414@qq.com>
2020-05-15 16:14:30 +0800

    5.15

--- src/servlet/User/UserRegisterServlet.java
+++ src/servlet/User/UserRegisterServlet.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import dao.UserDao;
 import dao.Impl.UserDaoImpl;
 import model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import servlet.JDBCUtils;
 
 /**
  * Servlet implementation class UserRegisterServlet
@@ -22,7 +24,7 @@ import model.User;
 @WebServlet("/UserRegisterServlet")
 public class UserRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+       JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType ("text/html;charset=UTF-8");
@@ -34,15 +36,27 @@ public class UserRegisterServlet extends HttpServlet {
 		String password2 = request.getParameter("password2");
 		String Phonenumber = request.getParameter("Phonenumber");
 		String backPassTip = request.getParameter("backPassTip");
-		
-
-		
-		
-		
+		for (int i = 0; i < Phonenumber.length();i++){
+			if(!Character.isDigit(Phonenumber.charAt(i))){
+				PrintWriter out = response.getWriter();
+				String a = URLEncoder.encode("号码必须是数字!", "UTF-8");
+				out.print("<script language='javascript'>alert(decodeURIComponent('"+a+"'));window.location.href='login.jsp?method=sindex';</script>");
+				out.flush();
+				out.close();
+				return;
+			}
+		}
+		if (name.length()==0 || password.length()==0 || password2.length()==0 || Phonenumber.length()==0 || backPassTip.length()==0||selfdescn.length()==0) {
+			PrintWriter out = response.getWriter();
+			String a = URLEncoder.encode("不能留空，请重试!", "UTF-8");
+			out.print("<script language='javascript'>alert(decodeURIComponent('"+a+"'));window.location.href='login.jsp?method=sindex';</script>");
+			out.flush();
+			out.close();
+			return;
+		}
 		UserDao userdao =new UserDaoImpl();
 		User user=userdao.queryById(Phonenumber);
 		if(user!=null) {
-		
 			 PrintWriter out = response.getWriter();
 			 String a = URLEncoder.encode("账号已存在，请重试!", "UTF-8"); 
 			 out.print("<script language='javascript'>alert(decodeURIComponent('"+a+"'));window.location.href='login.jsp?method=sindex';</script>");
@@ -51,10 +65,8 @@ public class UserRegisterServlet extends HttpServlet {
 	
 		}
 		else {
-
 			if(password.equals(password2)) {
 				user=new User();
-//				user.setId(Integer.parseInt(id));
 				user.setName(name);
 				user.setSelfdescn(selfdescn);
 				user.setPassword(password);
@@ -62,7 +74,6 @@ public class UserRegisterServlet extends HttpServlet {
 				user.setBackPassTip(backPassTip);
 				userdao.save(user);
 				request.getSession().setAttribute("loginUser", user);
-				
 				PrintWriter out = response.getWriter();
 				String a = URLEncoder.encode("注册成功！", "UTF-8"); 
 				out.print("<script language='javascript'>alert(decodeURIComponent('"+a+"'));window.location.href='index';</script>");
